c54f1fbffda31124d71c5de54807bf3db6524bac
MarcoAgosta <agostamarco@libero.it>
2022-10-27 16:52:33 +0200

    Creato un sistema di arrotondamento

--- JS/main.js
+++ JS/main.js
@@ -4,28 +4,37 @@ const eta = prompt("Inserire l'età del passeggero")
 let prezzoIniziale = kilometri*0.21
 
 if (eta > 65) {
+
     let sconto = prezzoIniziale*0.4
 
     let prezzoFinale = prezzoIniziale - sconto
 
+    let prezzoArrotondato = Math.round(prezzoFinale*100)/100
+
     const paragrafo = document.getElementById("paragrafo")
-    paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoFinale}€`
+    paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoArrotondato}€`
 
 } else if (eta < 18) {
+    
     let sconto = prezzoIniziale*0.2
 
     let prezzoFinale = prezzoIniziale - sconto
 
+    let prezzoArrotondato = Math.round(prezzoFinale*100)/100
+
     const paragrafo = document.getElementById("paragrafo")
-    paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoFinale}€`
+    paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoArrotondato}€`
 
 } else {
+    
     let sconto = 0
 
     let prezzoFinale = prezzoIniziale - sconto
 
+    let prezzoArrotondato = Math.round(prezzoFinale*100)/100
+
     const paragrafo = document.getElementById("paragrafo")
-    paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoFinale}€`
+    paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoArrotondato}€`
 
 }
 
