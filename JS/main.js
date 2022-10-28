9f4fc22e0d4ce92552ff627938b3bb19818ee15c
MarcoAgosta <agostamarco@libero.it>
2022-10-28 09:44:50 +0200

    Cambiati i let in const

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,39 @@
 const kilometri = prompt("Inserire numero di chilometri da percorrere")
 const eta = prompt("Inserire l'età del passeggero")
 
-let prezzoIniziale = kilometri*0.21
+const prezzoIniziale = kilometri*0.21
 
 if (eta > 65) {
 
-    let sconto = prezzoIniziale*0.4
+    const sconto = prezzoIniziale*0.4
 
-    let prezzoFinale = prezzoIniziale - sconto
+    const prezzoFinale = prezzoIniziale - sconto
 
-    let prezzoArrotondato = Math.round(prezzoFinale*100)/100
+    const prezzoArrotondato = Math.round(prezzoFinale*100)/100
 
     const paragrafo = document.getElementById("paragrafo")
     paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoArrotondato}€`
 
 } else if (eta < 18) {
     
-    let sconto = prezzoIniziale*0.2
+    const sconto = prezzoIniziale*0.2
 
-    let prezzoFinale = prezzoIniziale - sconto
+    const prezzoFinale = prezzoIniziale - sconto
 
-    let prezzoArrotondato = Math.round(prezzoFinale*100)/100
+    const prezzoArrotondato = Math.round(prezzoFinale*100)/100
 
     const paragrafo = document.getElementById("paragrafo")
     paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoArrotondato}€`
 
 } else {
     
-    let sconto = 0
+    const sconto = 0
 
-    let prezzoFinale = prezzoIniziale - sconto
+    const prezzoFinale = prezzoIniziale - sconto
 
-    let prezzoArrotondato = Math.round(prezzoFinale*100)/100
+    const prezzoArrotondato = Math.round(prezzoFinale*100)/100
 
     const paragrafo = document.getElementById("paragrafo")
     paragrafo.innerHTML = `Il prezzo del biglietto è ${prezzoArrotondato}€`
 
 }
-
